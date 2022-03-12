53ad91542ced45c1cd2ca8aa3dda9d7d65f0c2bc
Aaron J Lanning <64545232+dangeerraaron@users.noreply.github.com>
2022-03-11 19:18:57 -0500

    Update RecursionStaticVariable.c

--- RecursionStaticVariable.c
+++ RecursionStaticVariable.c
@@ -2,12 +2,12 @@
 
 int fun_1(int n){ //formal parameter that accepts the value of 'y'
     
-    static int y = 0; //fixed variable to increment
+    static int a = 0; //fixed variable to increment
     
     if(n > 0){ 
         
-        y++; //increment y by 1 
-        return fun_1(n-1) + y; // on return will add y to each function call as call stack record deleted
+        a++; //increment a by 1 
+        return fun_1(n-1) + a; // on return will add a to each function call as call stack record deleted
     
     }
     return 0;
